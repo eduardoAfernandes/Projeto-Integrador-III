241beae111ceb0caf02c05d388bfd6dcefccc1aa
Carlos Gustavo de Lacerda Stein <36887697+cgletras@users.noreply.github.com>
2019-04-29 22:30:58 -0300

    Mudamos o nome da tabela
    Mudamos o nome da tabela contem_esc para escritor_produto
    Mudamos o nome da tabela contem_pers para personagem_produto

--- banco de dados/LeilaoDeQuadrinhos.sql
+++ banco de dados/LeilaoDeQuadrinhos.sql
@@ -23,11 +23,35 @@ categoria VARCHAR(10), -- TPB, MENSAL
 numero_paginas INT, 
 peso INT, 
 capa_tipo VARCHAR (5), -- HARD, SOFT
-capa_imagem VARCHAR(20), -- ENDEREÇO IMAGEM
+capa_imagem VARCHAR(50), -- ENDEREÇO IMAGEM
 id_estado_produto INT, -- ATIVO, INATIVO, EMLEILAO, LEILOADO
 	FOREIGN KEY (id_estado_produto) REFERENCES Estado_produto(id_estado_produto),
 id_usuario INT,
 	FOREIGN KEY (id_usuario) REFERENCES Conta(id_usuario)
+);
+
+CREATE TABLE Escritor(
+	id_escritor INT PRIMARY KEY auto_increment,
+    nome VARCHAR(50)
+);
+
+CREATE TABLE escritor_produto(
+	id_escritor INT,
+		FOREIGN KEY(id_escritor) REFERENCES Escritor(id_escritor),
+    id_produto INT,
+		FOREIGN KEY(id_produto) REFERENCES Produto(id_produto)
+);
+
+CREATE TABLE Personagem(
+	id_personagem INT PRIMARY KEY auto_increment,
+    nome VARCHAR(50)
+);
+
+CREATE TABLE personagem_produto(
+	id_personagem INT,
+		FOREIGN KEY(id_personagem) REFERENCES Personagem(id_personagem),
+    id_produto INT,
+		FOREIGN KEY(id_produto) REFERENCES Produto(id_produto)
 );
 
 CREATE TABLE Estado_leilao(
@@ -68,17 +92,24 @@ VALUES
 ('Tiago Marques', 'ttiiaagguu@gmail.com', 'tiago2468', '1989-12-07', '1'),
 ('Willian de Freitas Oliveria', 'willian.freitasoliveira@gmail.com', 'willian2468', '1990-08-13', '1');
 
-INSERT INTO Produto(id_produto, editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario)
+INSERT INTO Estado_produto(estado)
 VALUES
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario),
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario),
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario),
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario),
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario),
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario),
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario),
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario),
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario),
-(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario);
+('ativo'), -- id 1
+('inativo'), -- id 2
+('em_leilao'), -- id 3
+('leiloado'); -- id 4
 
+INSERT INTO Estado_leilao(estado)
+VALUES
+('ativo'), -- id 1 
+('inativo'), -- id 2
+('espera'), -- id 3
+('finalizado'); -- id 4
 
+INSERT INTO Produto(editora, titulo, categoria, numero_paginas, peso, capa_tipo, capa_imagem, id_estado_produto, id_usuario)
+VALUES
+('Marvel', 'Onslaught 1 - The awakening', 'TPB', 73, 150, 'SOFT', '/imagens/capas/user/1/1.jpg', 1, 1),
+('DC', 'Secret Origins - Featuring JLA', 'TPB', 65, 145, 'SOFT', '/imagens/capas/user/1/2.jpg', 1, 1);
+
+RENAME TABLE contem_esc TO escritor_produto;
+RENAME TABLE contem_pers TO personagem_produto; 
