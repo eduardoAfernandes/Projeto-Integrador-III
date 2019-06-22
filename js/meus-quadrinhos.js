2cb7374266f779900d67adbd6b4258569f80a79d
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2019-06-22 10:33:24 -0300

    Alteracoes diversas no Front

--- js/meus-quadrinhos.js
+++ js/meus-quadrinhos.js
@@ -18,66 +18,66 @@ function verifyInput(id) {
     var regexPags = onlyNumber(nrPagina);
     var regexPeso = onlyNumber(peso);
 
-    if(id == 'títuloQuadrinho'){
-        if(!regexTitle){
+    if (id == 'títuloQuadrinho') {
+        if (!regexTitle) {
             document.getElementById('títuloQuadrinho').style.border = "3px solid red";
             document.getElementById('aviso-erro-titulo').classList.replace('d-none', 'd-block');
-        }else{
+        } else {
             document.getElementById('títuloQuadrinho').style.border = "3px solid lightgreen";
             document.getElementById('aviso-erro-titulo').classList.replace('d-block', 'd-none');
             validacaoTitulo = true;
         }
 
-    }else if(id == 'editora'){
-        if(editora == 'editora'){
+    } else if (id == 'editora') {
+        if (editora == 'editora') {
             document.getElementById('editora').style.border = "3px solid red";
             document.getElementById('aviso-erro-editora').classList.replace('d-none', 'd-block');
-        }else{
+        } else {
             document.getElementById('editora').style.border = "3px solid lightgreen";
             document.getElementById('aviso-erro-editora').classList.replace('d-block', 'd-none');
             validacaoEditora = true;
         }
-    }else if(id == 'categoria'){
-        if(categoria == 'categoria'){
+    } else if (id == 'categoria') {
+        if (categoria == 'categoria') {
             document.getElementById('categoria').style.border = "3px solid red";
             document.getElementById('aviso-erro-categoria').classList.replace('d-none', 'd-block');
-        }else{
+        } else {
             document.getElementById('categoria').style.border = "3px solid lightgreen";
             document.getElementById('aviso-erro-categoria').classList.replace('d-block', 'd-none');
             validacaoCategoria = true;
         }
-    }else if (id == 'numeroPags'){
-        if(nrPagina <= 0 ){
+    } else if (id == 'numeroPags') {
+        if (nrPagina <= 0) {
             document.getElementById('numeroPags').style.border = "3px solid red";
             document.getElementById('aviso-erro-paginas').classList.replace('d-none', 'd-block');
+        } else if (!regexPags) {
+            document.getElementById('numeroPags').style.border = "3px solid red";
             document.getElementById('aviso-erro-paginas').innerHTML = "Quantidade de páginas inválida!!"
-        }else if(!regexPags){
-            document.getElementById('numeroPags').style.border = "3px solid red";
             document.getElementById('aviso-erro-paginas').classList.replace('d-none', 'd-block');
-        }else{
+        } else {
             document.getElementById('numeroPags').style.border = "3px solid lightgreen";
             document.getElementById('aviso-erro-paginas').classList.replace('d-block', 'd-none');
             validacaoNrPagina = true;
 
         }
-    }else if(id == 'pesoQuadrinho'){
-        if (peso <= 0){
+    } else if (id == 'pesoQuadrinho') {
+        if (peso <= 0) {
             document.getElementById('pesoQuadrinho').style.border = "3px solid red";
             document.getElementById('aviso-erro-peso').classList.replace('d-none', 'd-block');
             document.getElementById('aviso-erro-peso').innerHTML = "Peso inválido!!"
-        }else if(!regexPeso){
+        } else if (!regexPeso) {
             document.getElementById('pesoQuadrinho').style.border = "3px solid red";
             document.getElementById('aviso-erro-peso').classList.replace('d-none', 'd-block');
-        }else{
+        } else {
             document.getElementById('pesoQuadrinho').style.border = "3px solid lightgreen";
             document.getElementById('aviso-erro-peso').classList.replace('d-block', 'd-none');
             validacaoPeso = true;
         }
-    } else if(id == 'tipoCapa'){
-        if (tipoCapa == 'capa'){
+    } else if (id == 'tipoCapa') {
+        if (tipoCapa == 'capa') {
             document.getElementById('tipoCapa').style.border = "3px solid red";
             document.getElementById('aviso-erro-capa').classList.replace('d-none', 'd-block');
-        }else{
+        } else {
             document.getElementById('tipoCapa').style.border = "3px solid lightgreen";
             document.getElementById('aviso-erro-capa').classList.replace('d-block', 'd-none');
             validacaoTipoCapa = true;
@@ -86,30 +86,30 @@ function verifyInput(id) {
 }
 
 
-function validForm(){
-    if(!validacaoTitulo){
+function validForm() {
+    if (!validacaoTitulo) {
         document.getElementById('títuloQuadrinho').style.border = "3px solid red";
         document.getElementById('aviso-erro-titulo').classList.replace('d-none', 'd-block');
-    }else if(!validacaoEditora){
+    } else if (!validacaoEditora) {
         document.getElementById('editora').style.border = "3px solid red";
         document.getElementById('aviso-erro-editora').classList.replace('d-none', 'd-block');
-    }else if (!validacaoCategoria){
+    } else if (!validacaoCategoria) {
         document.getElementById('categoria').style.border = "3px solid red";
-            document.getElementById('aviso-erro-categoria').classList.replace('d-none', 'd-block');
-    }else if(!validacaoNrPagina){
+        document.getElementById('aviso-erro-categoria').classList.replace('d-none', 'd-block');
+    } else if (!validacaoNrPagina) {
         document.getElementById('numeroPags').style.border = "3px solid red";
         document.getElementById('aviso-erro-paginas').classList.replace('d-none', 'd-block');
-    }else if(!validacaoPeso){
+    } else if (!validacaoPeso) {
         document.getElementById('pesoQuadrinho').style.border = "3px solid red";
         document.getElementById('aviso-erro-peso').classList.replace('d-none', 'd-block');
-    }else if(!validacaoTipoCapa){
+    } else if (!validacaoTipoCapa) {
         document.getElementById('tipoCapa').style.border = "3px solid red";
         document.getElementById('aviso-erro-capa').classList.replace('d-none', 'd-block');
     }
 }
 
 function validTitle(palavra) {
-    const filter_nome =/^([a-zA-Zà-úÀ-Ú]|\s+)+$/;
+    const filter_nome = /^([a-zA-Zà-úÀ-Ú]|\s+)+$/;
 
     if (!filter_nome.test(palavra)) {
         return false;
@@ -120,12 +120,42 @@ function validTitle(palavra) {
 
 }
 
-function onlyNumber(value){
+function onlyNumber(value) {
     const filter_number = /[0-9]$/
-    if(!filter_number.test(value)){
+    if (!filter_number.test(value)) {
         return false;
     }
-    if(filter_number.test(value)){
+    if (filter_number.test(value)) {
         return true;
     }
+}
+
+
+
+
+function verifyInputModalLeilao(id) {
+    var dataInput = document.getElementById('dt-inicio').value;
+    var duracaoLeilao = document.getElementById('duracao').value;
+    var valorInicial = document.getElementById('vlr-inicial').value;
+    var lancePadrao = document.getElementById('lancePadrao').value;
+
+    const now = moment();
+    dataInicio = moment(dataInput);
+    var validData = moment(dataInicio).isBefore(now);
+
+
+    if (id == 'dt-inicio') {
+        if (dataInput == '') {
+            document.getElementById('dt-inicio').style.border = "3px solid red";
+            document.getElementById('aviso-erro-data').classList.replace('d-none', 'd-block');
+            document.getElementById('aviso-erro-data').innerHTML = "Informe a data de inicio do leilão!!"
+        } else if (!validData || (dataInicio.get("date") == now.get("date") && (dataInicio.get("month") + 1) == (now.get("month") + 1) && dataInicio.get("year") == now.get("year"))) {
+            document.getElementById('dt-inicio').style.border = "3px solid lightgreen";
+            document.getElementById('aviso-erro-data').classList.replace('d-block', 'd-none');
+
+        }
+
+    }
+
+
 }
