c5bf277145b891ea0aa7fe129a91d67d3d068dc2
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2019-07-04 00:49:42 -0300

    Tabela meus lances efetuados

--- js/populateMyComicBooks.js
+++ js/populateMyComicBooks.js
@@ -1,3 +1,32 @@
+// Tooltips
+
+var tooltips = {
+    "titulo": "O titulo representa o nome do quadrinho",
+    "categoria": "A categoria do quadrinho define se ele é um TPB, ou seja, uma coleção ou se é um quadrinho de edição mensal. Passe o mouse sobre as opções para mais informações",
+    "categoriaTPB": "TPB é uma coleção de quadrinhos reunidas em uma só encadernação. É uma historia completa",
+    "categoriaMENSAL": "MENSAL quer dizer que esse quadrinho faz parte de uma publicação mensal, ou seja, tem um segmento de história",
+    "pesoQuadrinho": "Neste campo você deve listar quantos GRAMAS pesam o seu quadrinho. Digite somente o numero sem informar a unidade",
+    "editora": "Neste campo você deve selecionar qual a editora do seu quadrinho. Se não for Marvel ou DC, selecione OUTRAS",
+    "numeroPaginas": "Aqui você deve listar o numero de páginas do seu quadrinho. Digite somente numeros",
+    "tipoCapa": " Neste campo você entre o tipo de campa, entre HARD e SOFT, ou seja, capa DURA OU MOLE",
+    "tipoCapaMole": "SOFT é o tipo de capa MOLE",
+    "tipoCapaDura": "HARD é o tipo de capa DURA"
+};
+
+function carregarTooltip() {
+    document.getElementById('tpTitulo').title = tooltips.titulo;
+    document.getElementById('tpEditora').title = tooltips.editora;
+    document.getElementById('tpCategoria').title = tooltips.categoria;
+    document.getElementById('tpCategoriaTPB').title = tooltips.categoriaTPB;
+    document.getElementById('tpCategoriaMENSAL').title = tooltips.categoriaMENSAL;
+    document.getElementById('tpNPaginas').title = tooltips.numeroPaginas;
+    document.getElementById('tpPeso').title = tooltips.pesoQuadrinho;
+    document.getElementById('tpTipoCapa').title = tooltips.tipoCapa;
+    document.getElementById('tpTipoCapaDura').title = tooltips.tipoCapaDura;
+    document.getElementById('tpTipoCapaMole').title = tooltips.tipoCapaMole;
+}
+
+
 function loadDados() {
     var settings = {
         "async": true,
@@ -21,7 +50,7 @@ function loadDados() {
             localStorage.setItem('find-products-by-user', JSON.stringify(response));
             $('#tableData').show()
             $('#optionsTables').show();
-
+            populateTableParticipations();
 
             for (i = 0; i <= response.data.length; i++) {
                 // console.log('ID do Produto: '+response.data[i].productID)
@@ -62,21 +91,18 @@ function loadDados() {
                 }
                 
                 else {
-                    $("#tabela").append("<tr>" + 
-                    "<td class='text-center'><img src='" + response.data[i].coverImage + "' alt='' height='100'></img></td>" +
-                        "<td class='text-center'><h5>" + response.data[i].title + "</h5></td>" + "<td class='text-center'>" + response.data[i].productStatus.status + "<input type='hidden'  value='" + response.data[i].productID + "'>" + "</td>" +
-                        "<td class='text-center'>" + "<img class='btnModalProduto'" + "data-toggle='modal' data-id='" + response.data[i].productID + "' data-target='#modalDadosQuadrinho' " +
-                        "src='img/edit.png'" + "<input type='hidden'id='productID'  value='" + response.data[i].productID + "'>" + "</td>" +
-                        "<td class='text-center'>" + "<img class='btnModalChangeToActivate'"  + "data-toggle='modal' data-id='" + response.data[i].productID + "' data-target='#modalLeilao' " +
-                        "src='img/auction.png'" + "</td>" +  "<td class='text-center'>" + "<input type='hidden'id='productID'  value='" + response.data[i].productID + "'>"+"<img src='img/delete.png' class='btnDelectProduct'"+ 
-                        "data-toggle='modal' data-id='" + response.data[i].productID + "' data-target='#modalExcluiProduto' " +"</td>" +
-                        "</tr>");
+                        $("#tabela").append("<tr>" + 
+                        "<td class='text-center'><img src='" + response.data[i].coverImage + "' alt='' height='100'></img></td>" +
+                            "<td class='text-center'><h5>" + response.data[i].title + "</h5></td>" + "<td class='text-center'>" + response.data[i].productStatus.status + "<input type='hidden'  value='" + response.data[i].productID + "'>" + "</td>" +
+                            "<td class='text-center'>" + "<img class='btnModalProduto'" + "data-toggle='modal' data-id='" + response.data[i].productID + "' data-target='#modalDadosQuadrinho' " +
+                            "src='img/edit.png'" + "<input type='hidden'id='productID'  value='" + response.data[i].productID + "'>" + "</td>" +
+                            "<td class='text-center'>" + "<img class='btnModalChangeToActivate'"  + "data-toggle='modal' data-id='" + response.data[i].productID + "' data-target='#modalLeilao' " +
+                            "src='img/auction.png'" + "</td>" +  "<td class='text-center'>" + "<input type='hidden'id='productID'  value='" + response.data[i].productID + "'>"+"<img src='img/delete.png' class='btnDelectProduct'"+ 
+                            "data-toggle='modal' data-id='" + response.data[i].productID + "' data-target='#modalExcluiProduto' " +"</td>" +
+                            "</tr>");
                 }
-
-
-
             }
-        })
+        });
 
 
 
@@ -91,8 +117,11 @@ document.getElementById('tableData').style.display = 'none';
     document.getElementById('option-table-meus-quadrinhos').style.boxShadow = '0px 0px black';
     document.getElementById('textoTableQuadrinhos').style.display = 'block';
     document.getElementById('textoTableParticipations').style.display = 'none';
+  
 
+    
 }
+
 
 function changetableQuadrinhos(){
     document.getElementById('tableData').style.display = 'block';
@@ -106,6 +135,44 @@ function changetableQuadrinhos(){
 
 }
 
+function populateTableParticipations(){
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://webserver-leilao.azurewebsites.net/webserver-leilao/controller/find-bid-by-user",
+        "method": "GET",
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded",
+            "cache-control": "no-cache",
+        },
+        "xhrFields": {
+            "withCredentials": true
+        }
+    }
+    $.ajax(settings)
+    .done(function (response) {
+        return response      
+    })
+    .done(function (response) {
+        console.log(response)
+        localStorage.setItem('find-bids-by-user', JSON.stringify(response));
+        for (i = 0; i <= response.data.length; i++) {
+            $("#tabelaParticipationsDados").append(          
+            "<tr>" + 
+            "<td class='text-center'><img src='" + response.data[i].auction.product.coverImage + "' alt='' height='100'></img></td>"+
+            "<td class='text-center'>"+response.data[i].auction.product.title + "</td>" +
+            "<td class='text-center'>" + 'R$ ' + response.data[i].bidValue +"</td>"+
+            "<td class='text-center'>" + 'R$ ' + response.data[i].auction.currentValue +"</td>"+
+            "<td class='text-center'>"  + response.data[i].auction.auctionStatus.status +"</td>"+
+            "</tr>");
+        }
+    });
+
+}
 
 
-loadDados();
+
+function loadData(){
+    loadDados();
+    carregarTooltip();
+}
